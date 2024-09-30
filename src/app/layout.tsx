1fd181053e28c2a70f30b1fa6eb0933375928f77
edson2001 <djiedson413@gmai.com>
2024-09-30 09:43:11 +0100

    feat: create user organism

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,13 @@ import localFont from "next/font/local";
 import "./globals.css";
 import StoreProvider from "@/src/app/StoreProvider"
 import { Toaster } from 'react-hot-toast';
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
-});
+})
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
