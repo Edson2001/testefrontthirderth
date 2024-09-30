b8a6437db1cabef21af96cba21d3a1acd39ad549
edson2001 <djiedson413@gmai.com>
2024-09-30 09:55:42 +0100

    feat: ajust table

--- src/components/mocules/tble-user.tsx
+++ src/components/mocules/tble-user.tsx
@@ -4,7 +4,8 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import Divider from '@mui/material/Divider';
-
+import {iUser} from "@/src/app/@types/User.type"
+import {Trash} from "lucide-react"
 import { useMemo } from 'react';
 import {
     MaterialReactTable,
@@ -14,18 +15,18 @@ import {
 import { Button } from '@mui/material';
 interface CustomersTableProps {
     loading: boolean
-    onDelete: (item: any /* iTeacher */)=> void,
-    onEdit: (item: any)=> void,
-    fullRows: any[]
+    onDelete: (item: iUser)=> void,
+    onEdit: (item: iUser)=> void,
+    fullRows: iUser[]
 }
 
-export function TeacherUser({
+export function TableUser({
     fullRows,
     onDelete,
     onEdit,
     loading
 }: CustomersTableProps): React.JSX.Element {
-    const columns = useMemo<MRT_ColumnDef<any>[]>(
+    const columns = useMemo<MRT_ColumnDef<iUser>[]>(
         () => [
             {
                 accessorKey: 'name',
@@ -38,38 +39,13 @@ export function TeacherUser({
                 size: 150,
             },
             {
-                accessorKey: 'identification',
-                header: 'Documento',
-                size: 150,
-            },
-            {
-                accessorKey: 'email',
-                header: 'Email',
-                size: 150,
-            },
-            {
-                accessorKey: 'typeIdentification',
-                header: 'Tipo de identificação',
-                size: 150,
-            },
-            {
-                accessorKey: 'phone1',
-                header: 'Telefone',
-                size: 150,
-            },
-            {
-                accessorKey: 'address',
-                header: 'Endereço',
-                size: 150,
-            },
-            {
                 accessorKey: 'description',
                 header: 'Operações',
                 Cell: ({ row }) => {
                     return (
                         <>
                             <Button onClick={()=> onDelete(row.original)}>
-                               {/*  <Trash /> */}
+                                <Trash />
                             </Button>
                             <Button onClick={()=> onEdit(row.original)}>
                                {/*  <Pencil /> */}
--- src/components/organism/User.tsx
+++ src/components/organism/User.tsx
@@ -1,6 +1,15 @@
 'use client'
+import {TableUser} from "../mocules/tble-user"
 
-
-export default function User( ) {
-  return <></>
+export default function User() {
+  return (
+    <>
+      <TableUser 
+        fullRows={[]}
+        loading={false}
+        onDelete={()=> console.log("")}
+        onEdit={()=> console.log("")}
+      />
+    </>
+  )
 }
