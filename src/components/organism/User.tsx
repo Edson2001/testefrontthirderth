89cb2149baf5b06cc8fdc35bb0805ce24e0a09b3
edson2001 <djiedson413@gmai.com>
2024-09-30 10:48:49 +0100

    feat: add delete function

--- src/components/organism/User.tsx
+++ src/components/organism/User.tsx
@@ -1,19 +1,30 @@
 'use client'
 import { TableUser } from "../molecules/tble-user"
 import { useUser } from "@/src/hooks/user.hook"
+import Confirmation from "../molecules/Confirmation"
 
 export default function User() {
 
-  const { data, isLoading } = useUser()
+  const { data, isLoading, isConfirmationOpen,  isDeletingUser} = useUser()
 
   return (
     <main className="flex flex-col justify-center items-center w-full px-4">
       <h1 className="text-white tx-xl mb-5 mt-4 font-bold">Tabele de controle de usuários</h1>
+
       <TableUser
         fullRows={data ?? []}
         loading={isLoading}
         onDelete={() => console.log("")}
         onEdit={() => console.log("")}
+      />
+
+      <Confirmation 
+        handleClose={()=> console.log("")}
+        handleOk={()=> console.log("")}
+        headerInformation="Deletar"
+        information="Deseja deletar este usuário?"
+        loading={isDeletingUser}
+        open={isConfirmationOpen}
       />
     </main>
   )
--- src/hooks/user.hook.ts
+++ src/hooks/user.hook.ts
@@ -1,11 +1,62 @@
-import { useGetUsersQuery } from "@/src/services/user.service"
+import {useState} from "react"
+import {
+    useGetUsersQuery,
+    useCreateUserMutation,
+    useUpdateUserMutation,
+    useDeleteUserMutation
+} from "@/src/services/user.service"
 
 export const useUser = () => {
 
     const { data, isLoading } = useGetUsersQuery()
 
+
+    const [createUser, { isLoading: isCreatingUser, error: createUserError }] = useCreateUserMutation();
+
+    const [updateUser, { isLoading: isUpdatingUser, error: updateUserError }] = useUpdateUserMutation();
+    const [deleteUser, { isLoading: isDeletingUser, error: deleteUserError }] = useDeleteUserMutation();
+
+    const [selectedUserId, setSelectedUserId] = useState<string | null>(null);
+    const [isConfirmationOpen, setIsConfirmationOpen] = useState(false);
+
+    const handleDelete = (userId: string) => {
+        setSelectedUserId(userId);
+        setIsConfirmationOpen(true);
+    };
+
+    const confirmDelete = async () => {
+        if (selectedUserId) {
+            try {
+                await deleteUser(selectedUserId).unwrap();
+                console.log("Usuário deletado com sucesso");
+            } catch (error) {
+                console.error("Erro ao deletar o usuário:", error);
+            } finally {
+                setIsConfirmationOpen(false);
+            }
+        }
+    };
+
+    const handleClose = () => {
+        setIsConfirmationOpen(false);
+        setSelectedUserId(null);
+    };
+
     return {
         data,
-        isLoading
+        isLoading,
+        createUser,
+        updateUser,
+        deleteUser,
+        isCreatingUser,
+        isUpdatingUser,
+        isDeletingUser,
+        handleDelete,
+        confirmDelete,
+        handleClose,
+        createUserError,
+        updateUserError,
+        deleteUserError,
+        isConfirmationOpen
     }
 }
